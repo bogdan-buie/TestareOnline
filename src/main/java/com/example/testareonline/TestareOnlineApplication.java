3f9a395aabff09fb2cd5649e539de0ba99f48d72
bogdan-buie <buiebogdan8@gmail.com>
2023-11-06 10:42:56 +0200

    Primul commit

---
+++ src/main/java/com/example/testareonline/TestareOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.testareonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestareOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestareOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testareonline/TestareOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testareonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestareOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
